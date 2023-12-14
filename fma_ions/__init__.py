6801bc78933b924f322a5a368578f236332e01f2
elwaagaa <elias.walter.waagaard@cern.ch>
2023-12-14 14:48:26 +0100

    Removed unnecessary import

--- fma_ions/__init__.py
+++ fma_ions/__init__.py
@@ -1,6 +1,4 @@
 from .fma_ions import FMA
-#from .fma_data_classes import BeamParameters_PS, BeamParameters_SPS , Sequences
-
 
 from .sequence_classes_sps import SPS_sequence_maker, BeamParameters_SPS
 from .sequence_classes_ps import PS_sequence_maker, BeamParameters_PS
