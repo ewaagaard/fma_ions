fa3a42201ab149db030dd26739e1c5c309629129
elwaagaa <elias.walter.waagaard@cern.ch>
2023-12-14 14:48:00 +0100

    Removed unnecessary import

--- fma_ions/fma_ions.py
+++ fma_ions/fma_ions.py
@@ -29,7 +29,6 @@ plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 import NAFFlib
 
-#from .fma_data_classes import BeamParameters_PS, BeamParameters_SPS, Sequences
 from .sequence_classes_ps import PS_sequence_maker, BeamParameters_PS
 from .sequence_classes_sps import SPS_sequence_maker, BeamParameters_SPS
 from .resonance_lines import resonance_lines
