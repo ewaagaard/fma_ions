2f1f40b36f4ea2f0369aaa53d744a34fd3140e61
elwaagaa <elias.walter.waagaard@cern.ch>
2023-12-19 17:14:39 +0100

    Updated environment file

--- fma_ions/fma_ions.py
+++ fma_ions/fma_ions.py
@@ -13,21 +13,6 @@ from scipy.interpolate import griddata
 
 ##### Plot settings 
 import matplotlib.pyplot as plt
-
-"""
-SMALL_SIZE = 18
-MEDIUM_SIZE = 21
-BIGGER_SIZE = 26
-plt.rcParams["font.family"] = "serif"
-plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
-plt.rc('axes', titlesize=BIGGER_SIZE)    # fontsize of the axes title
-plt.rc('axes', labelsize=BIGGER_SIZE)    # fontsize of the x and y labels
-plt.rc('xtick', labelsize=MEDIUM_SIZE)   # fontsize of the tick labels
-plt.rc('ytick', labelsize=MEDIUM_SIZE)   # fontsize of the tick labels
-plt.rc('legend', fontsize=SMALL_SIZE)   # legend fontsize
-plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
-"""
-
 plt.rcParams.update(
     {
         "font.family": "serif",
