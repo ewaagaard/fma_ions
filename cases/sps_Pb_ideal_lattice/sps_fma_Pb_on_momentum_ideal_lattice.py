974e440e2fd784dbff927a85a55615c5aa0a476a
elwaagaa <elias.walter.waagaard@cern.ch>
2024-01-10 09:53:06 +0100

    Updated cases

--- cases/sps_Pb_ideal_lattice/sps_fma_Pb_on_momentum_ideal_lattice.py
+++ cases/sps_Pb_ideal_lattice/sps_fma_Pb_on_momentum_ideal_lattice.py
@@ -4,4 +4,4 @@ Generate SPS Pb ion FMA plot - ideal lattice, no momentum offset
 import fma_ions
 
 fma_sps = fma_ions.FMA(output_folder='output_Pb_on_momentum_ideal_lattice', z0=0., n_linear=200)
-fma_sps.run_SPS(load_tbt_data=False)
+fma_sps.run_SPS(load_tbt_data=True)
--- cases/sps_Pb_ideal_lattice/sps_fma_Pb_on_momentum_ideal_lattice_Qy_0dot19.py
+++ cases/sps_Pb_ideal_lattice/sps_fma_Pb_on_momentum_ideal_lattice_Qy_0dot19.py
@@ -4,4 +4,4 @@ Generate SPS Pb ion FMA plot - ideal lattice, with momentum offset
 import fma_ions
 
 fma_sps = fma_ions.FMA(output_folder='output_Pb_on_momentum_ideal_lattice_Qy_0dot19', z0=0.0, n_linear=200)
-fma_sps.run_SPS(Qy_frac=19, load_tbt_data=False)
+fma_sps.run_SPS(Qy_frac=19, load_tbt_data=True)
