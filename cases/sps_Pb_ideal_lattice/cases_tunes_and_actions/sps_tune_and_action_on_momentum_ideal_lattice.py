83a27b7b7a3d9a2b6c994e462ce1e667c4be7dca
elwaagaa <elias.walter.waagaard@cern.ch>
2024-01-10 10:28:56 +0100

    Updated tune and action example

--- cases/sps_Pb_ideal_lattice/cases_tunes_and_actions/sps_tune_and_action_on_momentum_ideal_lattice.py
+++ cases/sps_Pb_ideal_lattice/cases_tunes_and_actions/sps_tune_and_action_on_momentum_ideal_lattice.py
@@ -9,17 +9,18 @@ SPS Pb ions: plot tune over action from generated turn-by-turn data
 import fma_ions
 import numpy as np
 
-d_min = -11.0
+d_min = -11.5
 
 # On-momentum case - Qy = 0.25
-fma_sps = fma_ions.FMA(output_folder='output_Pb_on_momentum_ideal_lattice_0dot25', num_turns=1200, n_linear=5000, z0=0.)
-fma_sps.run_SPS(load_tbt_data=False, make_single_Jy_trace=True)
+fma_sps = fma_ions.FMA(output_folder='output_Pb_on_momentum_ideal_lattice_0dot25', num_turns=1200, 
+                       n_linear=5000, z0=0., plot_order=7)
+fma_sps.run_SPS(load_tbt_data=True, make_single_Jy_trace=True)
 
 # Load Twiss and plot normalized phase space
 sps_seq = fma_ions.SPS_sequence_maker()
 sps_line, twiss_sps = sps_seq.load_xsuite_line_and_twiss(Qy_frac=25)
 
-Jx, Jy, Qx, Qy, d = fma_sps.plot_tune_over_action(twiss_sps, also_show_plot=False, case_name='z0 = 0')
+Jx, Jy, Qx, Qy, d = fma_sps.plot_tune_over_action(twiss_sps, load_tune_data=True, also_show_plot=False, case_name='z0 = 0')
 
 # Select index with a diffusion coefficient higher than a certain threshold
 ind = np.where(d > d_min)[0]
