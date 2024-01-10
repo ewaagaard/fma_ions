3275f12ad807af6151fcdde5c4c5590bf5f8a80e
elwaagaa <elias.walter.waagaard@cern.ch>
2024-01-10 09:53:34 +0100

    Test for SPS octupolar component

---
+++ tests/sps/test_sps_octupolar_components.py
@@ -0,0 +1,24 @@
+"""
+Check octupolar k3 components from Twiss with SPS Pb sequence used for FMA 
+"""
+import fma_ions
+
+# Initialize FMA object
+fma_ps = fma_ions.FMA()
+
+# Test Twiss and tune adjustments of SPS 
+sps = fma_ions.SPS_sequence_maker()
+line = sps.generate_xsuite_seq()
+twiss = line.twiss()
+print('\nSPS beam: new tunes Qx = {:.4f}, Qy = {:.4f}\n'.format(twiss['qx'], twiss['qy']))
+ 
+my_dict = line.to_dict()
+d =  my_dict["elements"]
+
+# Print all octupolar components present
+for key, value in d.items():
+    if value['__class__'] == 'Multipole' and value['_order'] >= 3:
+        print('{}: knl = {}'.format(key, value['knl']))
+
+
+
