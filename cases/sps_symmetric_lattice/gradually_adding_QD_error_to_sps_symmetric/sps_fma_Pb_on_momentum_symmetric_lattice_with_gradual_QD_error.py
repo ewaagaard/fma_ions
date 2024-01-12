987c2f10c5b3ecb270c82f156f6526be7b8c81bf
elwaagaa <elias.walter.waagaard@cern.ch>
2024-01-12 17:48:34 +0100

    First drafts for QD error in symmetric SPS lattice

---
+++ cases/sps_symmetric_lattice/gradually_adding_QD_error_to_sps_symmetric/sps_fma_Pb_on_momentum_symmetric_lattice_with_gradual_QD_error.py
@@ -0,0 +1,9 @@
+"""
+Generate SPS Pb ion FMA plot - symmetric lattice without QFA and QDA, no momentum offset
+and then 
+
+"""
+import fma_ions
+
+fma_sps = fma_ions.FMA(output_folder='output_Pb_on_momentum_symmetric_lattice', z0=0., n_linear=200)
+fma_sps.run_SPS(load_tbt_data=False, use_symmetric_lattice=True)
