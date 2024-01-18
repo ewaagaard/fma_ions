76f197318d4cbb2e4d1b2a78e38b13a9f0e91377
elwaagaa <elias.walter.waagaard@cern.ch>
2024-01-18 18:34:49 +0100

    Adjusted tune ripple cases

--- cases/sps_Pb_ideal_lattice/tune_ripple/sps_tune_ripple_4e4_turns.py
+++ cases/sps_Pb_ideal_lattice/tune_ripple/sps_tune_ripple_4e4_turns.py
@@ -4,6 +4,17 @@ Tester script to generate SPS sequence with different tunes
 import fma_ions
 import xtrack as xt
 
-# First test qh_setvalue from MADX
-sps_ripple = fma_ions.Tune_Ripple_SPS(ripple_period=1000, num_turns=40000, n_linear=500, output_folder='output_tune_ripple')
-sps_ripple.run_ripple_and_analysis(load_tbt_data=False, install_SC_on_line=True, use_symmetric_lattice=False)
+# select resonance and action range we are interested in
+test_only_4Qx = True
+action_limits=[1.5e-6, 1.7e-6]
+
+if test_only_4Qx:
+    # First test qh_setvalue from MADX
+    sps_ripple = fma_ions.Tune_Ripple_SPS(ripple_period=1000, num_turns=40_000, n_linear=50, r_min=2.9, n_sigma=3.1, output_folder='output_4Qx')
+    sps_ripple.run_ripple_and_analysis(load_tbt_data=True, install_SC_on_line=True, action_limits=action_limits,
+                                       also_show_plot=True)
+else:
+    # First test qh_setvalue from MADX
+    sps_ripple = fma_ions.Tune_Ripple_SPS(ripple_period=1000, num_turns=40_000, n_linear=50, output_folder='output_tune_ripple')
+    sps_ripple.run_ripple_and_analysis(load_tbt_data=True, install_SC_on_line=True, action_limits=action_limits,
+                                   also_show_plot=True)
--- cases/sps_symmetric_lattice/tune_ripple/sps_tune_ripple_4e4_turns.py
+++ cases/sps_symmetric_lattice/tune_ripple/sps_tune_ripple_4e4_turns.py
@@ -1,9 +1,21 @@
 """
 Tester script to generate SPS sequence with different tunes
+- select to generate subset of particles on specific resonance
 """
 import fma_ions
 import xtrack as xt
 
-# First test qh_setvalue from MADX
-sps_ripple = fma_ions.Tune_Ripple_SPS(ripple_period=1000, num_turns=40_000, n_linear=500, output_folder='output_tune_ripple')
-sps_ripple.run_ripple_and_analysis(load_tbt_data=False, install_SC_on_line=True)
+# select resonance and action range we are interested in
+test_only_8Qx = True
+action_limits=[1.5e-6, 1.7e-6]
+
+if test_only_8Qx:
+    # First test qh_setvalue from MADX
+    sps_ripple = fma_ions.Tune_Ripple_SPS(ripple_period=1000, num_turns=40_000, n_linear=50, r_min=2.9, n_sigma=3.1, output_folder='output_8Qx')
+    sps_ripple.run_ripple_and_analysis(load_tbt_data=True, install_SC_on_line=True, action_limits=action_limits,
+                                       also_show_plot=False)
+else:
+    # First test qh_setvalue from MADX
+    sps_ripple = fma_ions.Tune_Ripple_SPS(ripple_period=1000, num_turns=40_000, n_linear=50, output_folder='output_tune_ripple')
+    sps_ripple.run_ripple_and_analysis(load_tbt_data=True, install_SC_on_line=True, action_limits=action_limits,
+                                   also_show_plot=True)
