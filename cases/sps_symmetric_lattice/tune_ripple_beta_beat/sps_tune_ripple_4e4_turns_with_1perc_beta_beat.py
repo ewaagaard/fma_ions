716447e05d7d19e307813af1d64ffb236211cc50
elwaagaa <elias.walter.waagaard@cern.ch>
2024-01-22 17:09:24 +0100

    more cases

--- cases/sps_symmetric_lattice/tune_ripple_beta_beat/sps_tune_ripple_4e4_turns_with_1perc_beta_beat.py
+++ cases/sps_symmetric_lattice/tune_ripple_beta_beat/sps_tune_ripple_4e4_turns_with_1perc_beta_beat.py
@@ -7,5 +7,5 @@ import xtrack as xt
 
 # First test qh_setvalue from MADX
 sps_ripple = fma_ions.Tune_Ripple_SPS(ripple_period=10_000, num_turns=40_000, n_linear=30, output_folder='output_tune_ripple_bb_0dot01')
-sps_ripple.run_ripple_and_analysis(load_tbt_data=False, install_SC_on_line=True, sextupolar_value_to_add=0.0, plot_random_colors=True,
+sps_ripple.run_ripple_and_analysis(load_tbt_data=True, install_SC_on_line=True, sextupolar_value_to_add=0.0, plot_random_colors=True,
                                also_show_plot=True, phase_sweep_up_to_turn=5000, phase_space_sweep_interval=50, beta_beat=0.01)
