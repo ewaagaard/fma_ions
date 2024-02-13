7aeca2ad86a237323abb00dd9e73fbc660174e78
elwaagaa <elias.walter.waagaard@cern.ch>
2024-02-13 10:55:42 +0100

    First tests for LEIR

---
+++ tests/leir/test_loading_leir_seq.py
@@ -0,0 +1,13 @@
+"""
+Tester script to load proper LEIR sequence
+"""
+import fma_ions
+
+leir = fma_ions.LEIR_sequence_maker()
+line, twiss = leir.generate_xsuite_seq()
+
+# Get gamma from proper reference particle
+beta0 = line.particle_ref['beta0']
+
+print('Generated LEIR Xsuite sequence with Qx={:.4f}, Qy={:4f}, dq1={:.4e}, dq2={:.4e}'.format(
+   twiss['qx'], twiss['qy'], twiss['dqx'] / beta0, twiss['dqy'] / beta0 ))
