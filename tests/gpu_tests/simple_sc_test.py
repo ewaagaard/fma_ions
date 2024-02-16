748dee1bf7ec73ceaeff09ec9552e6d4002e0e83
elias <elias.waagaard@gmail.com>
2024-02-16 12:06:13 +0100

    Successful GPU tests with tracking

--- tests/gpu_tests/simple_sc_test.py
+++ tests/gpu_tests/simple_sc_test.py
@@ -8,28 +8,40 @@ import xobjects as xo
 import xtrack as xt
 import xfields as xf
 
+
+# Initial parameters
+n_part = 100
+n_turns = 100
+gpu_device = 0
+
+# Switch context if needed
+if gpu_device is None:
+   context = xo.ContextCpu()
+   print('Attempting CPUs')
+else:
+   context = xo.ContextCupy(device=gpu_device)
+   print('Attempting GPUs')
+
 # Import SPS line
 sps = fma_ions.SPS_sequence_maker()
 line, twiss = sps.load_xsuite_line_and_twiss()
 
-# Choose context
-context = xo.ContextCupy()
+# Choose context - discard tracker and move context to GPU
+context = xo.ContextCupy(device=gpu_device)
 line.discard_tracker()
 line.build_tracker(_context=context)
 
 ## Build particle object on context
-n_part = 20
-particles = line.build_particles(
-                        x=np.random.uniform(-1e-3, 1e-3, n_part),
-                        px=np.random.uniform(-1e-5, 1e-5, n_part),
-                        y=np.random.uniform(-2e-3, 2e-3, n_part),
-                        py=np.random.uniform(-3e-5, 3e-5, n_part),
-                        zeta=np.random.uniform(-1e-2, 1e-2, n_part),
-                        delta=np.random.uniform(-1e-4, 1e-4, n_part))
+particles = xp.generate_matched_gaussian_bunch(_context=context,
+        num_particles=n_part, total_intensity_particles=fma_ions.BeamParameters_SPS.Nb,
+        nemitt_x=fma_ions.BeamParameters_SPS.exn, nemitt_y=fma_ions.BeamParameters_SPS.eyn, 
+        sigma_z= fma_ions.BeamParameters_SPS.sigma_z,
+        particle_ref=line.particle_ref, line=line)
 # Reference mass, charge, energy are taken from the reference particle.
 # Particles are allocated on the context chosen for the line.
 
-## Track (no saving of turn-by-turn data)
-n_turns = 100
-line.track(particles, num_turns=n_turns)
+for turn in range(n_turns):
+   line.track(particles)
+   print('Tracking turn {}'.format(turn))
+
 
