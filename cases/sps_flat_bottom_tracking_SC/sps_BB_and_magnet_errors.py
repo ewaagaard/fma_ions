1baac58560fc312554d5a2195a802b37ceb641fd
elias <elias.waagaard@gmail.com>
2024-03-12 18:09:25 +0100

    updated case parameters

--- cases/sps_flat_bottom_tracking_SC/sps_BB_and_magnet_errors.py
+++ cases/sps_flat_bottom_tracking_SC/sps_BB_and_magnet_errors.py
@@ -4,5 +4,5 @@ SPS lattice with BB, non-linear magnet errors and space charge
 import fma_ions
 
 # Test default tracking with space charge on GPU context - then test plotting
-sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=50_000, output_folder='output_BB_and_magnet_errors')
+sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=10_000, output_folder='output_BB_and_magnet_errors')
 tbt = sps.track_SPS(which_context='gpu', add_aperture=True, beta_beat=0.1, add_non_linear_magnet_errors=True)
--- cases/sps_flat_bottom_tracking_SC/sps_ideal_lattice.py
+++ cases/sps_flat_bottom_tracking_SC/sps_ideal_lattice.py
@@ -5,5 +5,5 @@ SPS ideal lattice with space charge
 import fma_ions
 
 # Test default tracking with space charge on GPU context - then test plotting
-sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=50_000, output_folder='output_ideal_lattice')
+sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=10_000, output_folder='output_ideal_lattice')
 tbt = sps.track_SPS(which_context='gpu', add_aperture=True)
