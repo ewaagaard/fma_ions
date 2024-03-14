5ffabf1141d4175c33c96e303694870a319b47cf
elias <elias.waagaard@gmail.com>
2024-03-14 09:52:43 +0100

    updated tests

--- tests/flat_bottom_tracking/tests_for_htcondor/test_cpu_submission/sps_ideal_lattice_sc.py
+++ tests/flat_bottom_tracking/tests_for_htcondor/test_cpu_submission/sps_ideal_lattice_sc.py
@@ -1,9 +1,11 @@
 """
 SPS ideal lattice with space charge - CPU for HTCondor submission
 """
-
 import fma_ions
+import pandas as pd
+output_dir = './'
 
 # Test default tracking with space charge on CPU context - then test plotting
-sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=100, output_folder='output_ideal_lattice')
+sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=100)
 tbt = sps.track_SPS(which_context='cpu')
+tbt.to_parquet(f'{output_dir}/tbt.parquet')
