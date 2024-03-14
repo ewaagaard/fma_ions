f22b38b9d9847235f8cbe388a82833b25f2948e3
elias <elias.waagaard@gmail.com>
2024-03-14 16:27:24 +0100

    case for 800K turns

--- htcondor_submission/long_SPS_flat_bottom_tracking/800k_turns_gpu/sps_ideal_lattice_sc.py
+++ htcondor_submission/long_SPS_flat_bottom_tracking/800k_turns_gpu/sps_ideal_lattice_sc.py
@@ -1,11 +1,13 @@
 """
-SPS ideal lattice with space charge - GPU for HTCondor submission
+SPS ideal lattice with space charge - with GPUs for 800 000 turns
 """
 import fma_ions
 import pandas as pd
 output_dir = './'
 
+n_turns = 800_000
+
 # Test default tracking with space charge on CPU context - then test plotting
-sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=100)
+sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=n_turns)
 tbt = sps.track_SPS(which_context='gpu')
 tbt.to_parquet(f'{output_dir}/tbt.parquet')
