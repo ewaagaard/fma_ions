012a9ef263cf83991bea8690d04beb9d0a5e791a
elias <elias.waagaard@gmail.com>
2024-03-15 16:52:40 +0100

    long tracking with tune ripple

--- htcondor_submission/long_SPS_flat_bottom_tracking/2M_turns_gpu_tune_ripple/launch_gpu.py
+++ htcondor_submission/long_SPS_flat_bottom_tracking/2M_turns_gpu_tune_ripple/launch_gpu.py
@@ -1,5 +1,5 @@
 """
-Launcher script to HTCondor for three SPS cases - with GPUs for 2M turns
+Launcher script to HTCondor for three SPS cases with tune ripple - with GPUs for 2M turns
 """
 import fma_ions
 import os
@@ -7,9 +7,9 @@ import pathlib
 
 # Find path of script being run
 dir_path = pathlib.Path(__file__).parent.absolute()
-script_names = ['sps_ideal_lattice_sc.py', 'sps_BB_sc.py', 'sps_BB_sc_ibs.py', 'sps_BB_only_ibs.py']
-folder_names = ['sps_ideal_lattice_2M_turns', 'sps_BB_2M_turns', 'sps_BB_sc_ibs_2M_turns', 'sps_BB_only_ibs_2M_turns']
-string_array = ['SC ideal lattice', 'SC with BB', 'SC + IBS with BB', 'IBS with BB']    
+script_names = ['sps_ideal_lattice_sc.py', 'sps_BB_sc.py', 'sps_BB_sc_ibs.py', 'sps_BB_only_ibs.py', 'sps_BB_sc_ibs_no_ripple.py']
+folder_names = ['sps_ideal_lattice_2M_turns', 'sps_BB_2M_turns', 'sps_BB_sc_ibs_2M_turns', 'sps_BB_only_ibs_2M_turns', 'sps_BB_sc_ibs_no_ripple_2M_turns']
+string_array = ['SC ideal lattice + tune ripple', 'SC with BB + tune ripple', 'SC + IBS with BB + tune ripple', 'IBS with BB + tune ripple', 'SC + IBS with BB, no ripple']    
 
 # Instantiate the submitter class and launch the two jobs
 sub = fma_ions.Submitter() 
@@ -19,6 +19,5 @@ for i, script in enumerate(script_names):
     file_name = os.path.join(dir_path, script)
     print(f"Submitting {file_name}")
     sub.submit_GPU(file_name, extra_output_name_str=folder_names[i], number_of_turn_string='2M_turns', job_flavour='nextweek')
-sub.output_folder_eos = '/eos/user/e/elwaagaa/PhD/Projects/fma_ions/htcondor_submission/output'
 sub.copy_master_plot_script(folder_names, string_array)
 
--- htcondor_submission/long_SPS_flat_bottom_tracking/2M_turns_gpu_tune_ripple/sps_BB_only_ibs.py
+++ htcondor_submission/long_SPS_flat_bottom_tracking/2M_turns_gpu_tune_ripple/sps_BB_only_ibs.py
@@ -1,5 +1,5 @@
 """
-SPS lattice with beta-beat and only IBS (no SC) - with GPUs for 2M turns
+SPS lattice with beta-beat and only IBS (no SC) and tune ripple  - with GPUs for 2M turns
 """
 import fma_ions
 import pandas as pd
@@ -10,5 +10,5 @@ n_turns = 2_000_000
 # Test default tracking with space charge on CPU context - then test plotting
 sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=n_turns)
 tbt = sps.track_SPS(which_context='gpu', install_SC_on_line=False, beta_beat=0.1, add_non_linear_magnet_errors=True, 
-                    apply_kinetic_IBS_kicks=True, ibs_step = 300)
+                    apply_kinetic_IBS_kicks=True, ibs_step = 300, add_tune_ripple=True)
 tbt.to_parquet(f'{output_dir}/tbt.parquet')
