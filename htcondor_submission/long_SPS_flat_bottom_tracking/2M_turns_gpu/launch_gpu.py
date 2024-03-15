6f41767368fdffc22fe28e612cc5e583e7771562
elias <elias.waagaard@gmail.com>
2024-03-15 16:39:49 +0100

    added needed attribute

--- htcondor_submission/long_SPS_flat_bottom_tracking/2M_turns_gpu/launch_gpu.py
+++ htcondor_submission/long_SPS_flat_bottom_tracking/2M_turns_gpu/launch_gpu.py
@@ -19,5 +19,6 @@ sub = fma_ions.Submitter()
 #    file_name = os.path.join(dir_path, script)
 #    print(f"Submitting {file_name}")
 #    sub.submit_GPU(file_name, extra_output_name_str=folder_names[i], number_of_turn_string='2M_turns', job_flavour='nextweek')
+sub.output_folder_eos = '/eos/user/e/elwaagaa/PhD/Projects/fma_ions/htcondor_submission/output'
 sub.copy_master_plot_script(folder_names, string_array)
 
