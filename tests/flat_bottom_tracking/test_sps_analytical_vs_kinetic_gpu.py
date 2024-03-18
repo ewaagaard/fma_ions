a8ae195f080a282eec8407f18e45abf3a61abea1
elwaagaa <elias.walter.waagaard@cern.ch>
2024-03-18 17:12:02 +0100

    fixed correct contect

--- tests/flat_bottom_tracking/test_sps_analytical_vs_kinetic_gpu.py
+++ tests/flat_bottom_tracking/test_sps_analytical_vs_kinetic_gpu.py
@@ -8,5 +8,5 @@ import fma_ions
 sps = fma_ions.SPS_Flat_Bottom_Tracker(num_part=5000, num_turns=10_000, turn_print_interval=10)
 
 # First run with ideal lattice, then with beta-beat
-sps.run_analytical_vs_kinetic_emittance_evolution(Qy_frac=19)
-sps.run_analytical_vs_kinetic_emittance_evolution(Qy_frac=19, beta_beat=0.1, add_non_linear_magnet_errors=True)
+sps.run_analytical_vs_kinetic_emittance_evolution(Qy_frac=19, which_context='gpu')
+sps.run_analytical_vs_kinetic_emittance_evolution(Qy_frac=19, which_context='gpu', beta_beat=0.1, add_non_linear_magnet_errors=True)
