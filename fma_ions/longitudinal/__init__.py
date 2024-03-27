2a84eea5aeb8912bceeb2b71cc3baa4fa216a93b
elias <elias.waagaard@gmail.com>
2024-03-27 18:32:33 +0100

    first organization of longitudinal part

---
+++ fma_ions/longitudinal/__init__.py
@@ -0,0 +1,5 @@
+from .parabolic_longitudinal import generate_parabolic_distribution
+from .generate_longitudinal import (generate_longitudinal_coordinates,
+                                    _characterize_line)
+from .single_rf_harmonic_matcher import SingleRFHarmonicMatcher
+from .generate_binomial_longitudinal_distribution import binomial_longitudinal_distribution
