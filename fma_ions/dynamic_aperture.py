a55c7d0227fea958888b2004b18cb14a7a5bc7d0
elias <elias.waagaard@gmail.com>
2024-03-28 16:13:33 +0100

    fixed redundant import

--- fma_ions/dynamic_aperture.py
+++ fma_ions/dynamic_aperture.py
@@ -11,9 +11,8 @@ import xpart as xp
 import xobjects as xo
 
 from .fma_ions import FMA
-from .fma_data_classes import BeamParameters_PS, BeamParameters_SPS, Sequences
-from .sequence_classes_ps import PS_sequence_maker
-from .sequence_classes_sps import SPS_sequence_maker
+from .sequence_classes_ps import PS_sequence_maker, BeamParameters_PS
+from .sequence_classes_sps import SPS_sequence_maker, BeamParameters_SPS
 
 @dataclass
 class Dynamic_Aperture: 
