d8ef43800926c515d229cc7b2b2db93de3780f44
elias <elias.waagaard@gmail.com>
2024-03-29 09:44:41 +0100

    tests and submission scripts for binomial

---
+++ htcondor_submission/long_SPS_flat_bottom_binomial/50k_turns_gpu/sps_BB_sc.py
@@ -0,0 +1,12 @@
+"""
+SPS lattice with beta-beat and space charge - with GPUs for 50 000 turns
+"""
+import fma_ions
+import pandas as pd
+output_dir = './'
+
+# Instantiate SPS Flat Bottom Tracker and then track on GPUs
+sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=50_000)
+tbt = sps.track_SPS(which_context='gpu', Qy_frac=19, beta_beat=0.1, distribution_type='binomial',
+                    add_non_linear_magnet_errors=True)
+tbt.to_parquet(f'{output_dir}/tbt.parquet')
