4944638fbd6bd8ffdb74c53908abfae582e9413e
elias <elias.waagaard@gmail.com>
2024-03-29 09:45:53 +0100

    fixed show plot

--- tests/particle_distributions/test_tracking_binomial_cpu.py
+++ tests/particle_distributions/test_tracking_binomial_cpu.py
@@ -6,5 +6,5 @@ import fma_ions
 # Test default tracking with space charge on GPU context - then test plotting
 sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=100, turn_print_interval=10)
 tbt = sps.track_SPS(which_context='cpu', distribution_type='binomial', add_aperture=True)
-sps.plot_tracking_data(tbt, show_plot=True)
+sps.plot_tracking_data(tbt, show_plot=False)
 
