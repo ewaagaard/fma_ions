2b34f5fecc20fe89031db25a88d4428e51708f79
elias <elias.waagaard@gmail.com>
2024-04-15 16:07:23 +0200

    first on-momentum FMA for oxygen

---
+++ cases/sps_fma_O_ideal_lattice/on_momentum/sps_O_on_momentum_lower_Nb.py
@@ -0,0 +1,10 @@
+"""
+Generate SPS O ion FMA plot - ideal lattice, no momentum offset - lower Nb to better resolve tune shift
+"""
+import fma_ions
+
+beamParams = fma_ions.BeamParameters_SPS()
+beamParams.Nb = 1e9
+
+fma_sps = fma_ions.FMA(output_folder='output_O_on_momentum_ideal_lattice_lower_Nb', z0=0., n_linear=100)
+fma_sps.run_SPS(load_tbt_data=False, ion_type='O', which_context='gpu', beamParams=beamParams)
