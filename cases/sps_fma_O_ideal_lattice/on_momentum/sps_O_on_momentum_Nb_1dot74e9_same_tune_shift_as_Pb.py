386a243d18cd50ac11d550164eb5a96b21de8e03
elias <elias.waagaard@gmail.com>
2024-04-16 11:51:35 +0200

    tune spread cases

--- cases/sps_fma_O_ideal_lattice/on_momentum/sps_O_on_momentum_Nb_1dot74e9_same_tune_shift_as_Pb.py
+++ cases/sps_fma_O_ideal_lattice/on_momentum/sps_O_on_momentum_Nb_1dot74e9_same_tune_shift_as_Pb.py
@@ -7,5 +7,5 @@ import fma_ions
 beamParams = fma_ions.BeamParameters_SPS()
 beamParams.Nb = 1.74e9
 
-fma_sps = fma_ions.FMA(output_folder='output_O_on_momentum_ideal_lattice_same_dQ_as_Pb', z0=0., n_linear=100)
+fma_sps = fma_ions.FMA(output_folder='output_O_on_momentum_ideal_lattice_Nb_1dot74e9_same_dQ_as_Pb', z0=0., n_linear=100)
 fma_sps.run_SPS(load_tbt_data=False, ion_type='O', which_context='gpu', beamParams=beamParams)
