9a949eb6b46cc3e0d3e083d94b962e0791df573d
elwaagaa <elias.walter.waagaard@cern.ch>
2024-05-28 15:08:56 +0200

    syntax fix

--- tests/sps/test_generating_sps_seq.py
+++ tests/sps/test_generating_sps_seq.py
@@ -4,6 +4,6 @@ Tester script to generate SPS sequence with different tunes
 import fma_ions
 
 sps = fma_ions.SPS_sequence_maker(26.30, 26.19)
-line = sps.generate_xsuite_seq(save_xsuite_seq=False)
+line = sps.generate_xsuite_seq(save_xsuite_seq=True)
 twiss = line.twiss()
 print('\nGenerated sequence with qx = {}, qy = {}\n'.format(twiss['qx'], twiss['qy']))
