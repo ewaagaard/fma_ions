9fe8b4bfe305441754f4050b185755fff8a785df
elias <elias.waagaard@gmail.com>
2024-06-03 18:23:53 +0200

    more htcondor cases

--- htcondor_submission/longitudinal_losses/475k_turns_gpu_binomial_IBS_vs_measurements_after_RF_spill/launch_gpu.py
+++ htcondor_submission/longitudinal_losses/475k_turns_gpu_binomial_IBS_vs_measurements_after_RF_spill/launch_gpu.py
@@ -1,5 +1,5 @@
 """
-Launcher script to HTCondor for three SPS cases - with GPUs for 2M turns
+Launcher script to HTCondor for three SPS cases - with GPUs
 """
 import fma_ions
 import os
@@ -8,7 +8,7 @@ import pathlib
 # Find path of script being run
 dir_path = pathlib.Path(__file__).parent.absolute()
 script_names = ['sps_BB_only_ibs_binomial.py', 'sps_BB_only_ibs_binomial_emittances.py']
-folder_names = ['sps_BB_only_ibs_binomial_955k_turns', 'sps_BB_only_ibs_binomial_emittances_955k_turns']
+folder_names = ['sps_BB_only_ibs_binomial_475k_turns', 'sps_BB_only_ibs_binomial_emittances_475k_turns']
 string_array = ['IBS with BB binomial', 'IBS with BB binomial']    
 
 # Instantiate the submitter class and launch the two jobs
@@ -18,7 +18,7 @@ sub = fma_ions.Submitter()
 for i, script in enumerate(script_names):
     file_name = os.path.join(dir_path, script)
     print(f"Submitting {file_name}")
-    sub.submit_GPU(file_name, extra_output_name_str=folder_names[i], number_of_turn_string='955k_turns', job_flavour='nextweek', 
+    sub.submit_GPU(file_name, extra_output_name_str=folder_names[i], number_of_turn_string='475k_turns', job_flavour='nextweek', 
                    output_format='json')
 sub.copy_master_plot_script(folder_names, string_array)
 
