bf139eae75bde50bce0bc04b085adacb12819c1f
elias <elias.waagaard@gmail.com>
2024-06-05 14:35:47 +0200

    Gpu tests

--- tests/beam_profile_monitors/longitudinal/test_longitudinal_profile_monitors_gpu.py
+++ tests/beam_profile_monitors/longitudinal/test_longitudinal_profile_monitors_gpu.py
@@ -1,5 +1,5 @@
 """
-Simple test script to launch SPS tracking with longitudinal beam monitor
+Simple test script to launch SPS tracking with longitudinal beam monitor - on GPU context
 """
 import fma_ions
 output_dir = './'
@@ -9,7 +9,7 @@ num_part = 10_000
 
 # Test default tracking with space charge on CPU context - then test plotting
 sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=n_turns, num_part=num_part, turn_print_interval=20)
-zeta_monitor = sps.track_SPS(which_context='cpu', install_SC_on_line=False, add_aperture=False)
+zeta_monitor = sps.track_SPS(which_context='gpu', install_SC_on_line=False, add_aperture=False)
 
 sps_plot = fma_ions.SPS_Plotting()
 sps_plot.plot_longitudinal_monitor_data(zeta_monitor)
