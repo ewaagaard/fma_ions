7d71cd3c4a28fea09767c075a0251c04339f71a4
elwaagaa <elias.walter.waagaard@cern.ch>
2024-06-06 14:21:43 +0200

    fixed copying

--- htcondor_submission/launch_with_beam_profile_monitors/small_launch_tests/500_turns_test_cpu/sps_BB_only_ibs.py
+++ htcondor_submission/launch_with_beam_profile_monitors/small_launch_tests/500_turns_test_cpu/sps_BB_only_ibs.py
@@ -21,4 +21,4 @@ Qy_frac = 25 # old fractional tune
 sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=n_turns, num_part=num_part)
 tbt = sps.track_SPS(which_context='cpu', Qy_frac=Qy_frac, beamParams=beamParams, install_SC_on_line=False, beta_beat=0.1, 
                     add_non_linear_magnet_errors=True, apply_kinetic_IBS_kicks=True, ibs_step = 5000)
-tbt.to_json(f'{output_dir}/tbt.json')
+tbt.to_json(output_dir)
