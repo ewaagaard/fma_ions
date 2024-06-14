534213b4a191e388be964604baff276c3aed7f6e
elias <elias.waagaard@gmail.com>
2024-06-14 17:57:40 +0200

    HTCondor submission Q20 optics

---
+++ htcondor_submission/new_launch_cases/SPS_IBS_cases_vs_wall_current_monitor/955k_turns_gpu_binomial_IBS_vs_measurements_Q20/sps_ideal_lattice_only_ibs_binomial_before_RF_spill.py
@@ -0,0 +1,23 @@
+"""
+SPS lattice with ideal lattice and only IBS (no SC) - with GPUs
+Start with initial bunch length, before RF spill
+"""
+import fma_ions
+import pandas as pd
+output_dir = './'
+
+n_turns = 955_000  # corresponds to 22 s
+num_part = 10_000
+
+# Instantiate beam parameters, custom made to compare with 2016 measurements
+beamParams = fma_ions.BeamParameters_SPS()
+beamParams.Nb = 3.536e8  # loss factor from first turn observed with wall current monitor, before RF spill
+beamParams.exn = 1.3e-6 # in m
+beamParams.eyn = 0.8e-6 # in m
+Qy_frac = 25 # old fractional tune
+
+# Test default tracking with space charge on CPU context - then test plotting
+sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=n_turns, num_part=num_part, qx0=20.3, qy0=20.25, proton_optics='q20')
+tbt = sps.track_SPS(which_context='gpu', Qy_frac=Qy_frac, beamParams=beamParams, install_SC_on_line=False, apply_kinetic_IBS_kicks=True, 
+                    ibs_step = 5000, distribution_type='binomial')
+tbt.to_json(output_dir)
