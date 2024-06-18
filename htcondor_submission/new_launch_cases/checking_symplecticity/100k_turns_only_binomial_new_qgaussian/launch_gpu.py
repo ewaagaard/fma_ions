6a423ebc19aafee19acd9413e6a5b145cad692fc
elias <elias.waagaard@gmail.com>
2024-06-18 18:29:05 +0200

    new htcondor cases

---
+++ htcondor_submission/new_launch_cases/checking_symplecticity/100k_turns_only_binomial_new_qgaussian/launch_gpu.py
@@ -0,0 +1,26 @@
+"""
+Launcher script to HTCondor for SPS cases with GPUs
+"""
+import fma_ions
+import os
+import pathlib
+
+# Find path of script being run
+dir_path = pathlib.Path(__file__).parent.absolute()
+script_names = ['sps_BB_sc_binomial_Q20.py', 'sps_BB_sc_binomial_Q26.py', 
+                'sps_ideal_lattice_sc_binomial_Q20.py', 'sps_ideal_lattice_sc_binomial_Q26.py']
+folder_names = ['sps_BB_sc_binomial_Q20', 'sps_BB_sc_binomial_Q26',
+                'sps_ideal_lattice_sc_binomial_Q20', 'sps_ideal_lattice_sc_binomial_Q26']
+string_array = ['BB: SC binomial Q20', 'BB: SC binomial Q26', 
+                'Ideal lattice: SC binomial Q20', 'Ideal lattice: SC binomial Q26']    
+
+# Instantiate the submitter class and launch the two jobs
+sub = fma_ions.Submitter() 
+
+# Launch the Python scripts in this folder
+for i, script in enumerate(script_names):
+    file_name = os.path.join(dir_path, script)
+    print(f"Submitting {file_name}")
+    sub.submit_GPU(file_name, extra_output_name_str=folder_names[i], number_of_turn_string='100k_turns', job_flavour='nextweek')
+sub.copy_master_plot_script(folder_names, string_array)
+
---
+++ htcondor_submission/new_launch_cases/checking_symplecticity/100k_turns_only_binomial_new_qgaussian/sps_ideal_lattice_sc_binomial_Q26.py
@@ -0,0 +1,23 @@
+"""
+SPS lattice with ideal lattice and SC - with GPUs
+"""
+import fma_ions
+import pandas as pd
+output_dir = './'
+
+n_turns = 100_000
+num_part = 10_000
+
+# Instantiate beam parameters, custom made to compare with 2016 measurements
+beamParams = fma_ions.BeamParameters_SPS()
+beamParams.Nb = 3.536e8 * 0.95  # loss factor from first turn observed with wall current monitor
+beamParams.exn = 1.3e-6 # in m
+beamParams.eyn = 0.8e-6 # in m
+beamParams.sigma_z_binomial = 0.225 # what we measure after initial losses out of the RF bucket
+beamParams.m = 2.8 # meausred for binomial after SPS injection
+
+# Test default tracking with space charge on CPU context - then test plotting
+sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=n_turns, num_part=num_part)
+tbt = sps.track_SPS(which_context='gpu', beamParams=beamParams, install_SC_on_line=True, 
+                    apply_kinetic_IBS_kicks=False, distribution_type='binomial')
+tbt.to_json(output_dir)
