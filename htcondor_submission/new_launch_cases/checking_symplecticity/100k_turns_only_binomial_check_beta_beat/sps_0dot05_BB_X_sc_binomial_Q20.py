31cc214645ff861de5a8b804d03dc746198bd905
elwaagaa <elias.walter.waagaard@cern.ch>
2024-06-19 20:01:21 +0200

    new beta-beat caswe

--- htcondor_submission/new_launch_cases/checking_symplecticity/100k_turns_only_binomial_check_beta_beat/sps_0dot05_BB_X_sc_binomial_Q20.py
+++ htcondor_submission/new_launch_cases/checking_symplecticity/100k_turns_only_binomial_check_beta_beat/sps_0dot05_BB_X_sc_binomial_Q20.py
@@ -5,11 +5,11 @@ import fma_ions
 import pandas as pd
 output_dir = './'
 
-n_turns = 100_000
+n_turns = 100_000 
 num_part = 10_000
 
 # Test default tracking with space charge on CPU context - then test plotting
-sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=n_turns, num_part=num_part)
-tbt = sps.track_SPS(which_context='gpu', install_SC_on_line=True, beta_beat=0.1, add_non_linear_magnet_errors=True,
+sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=n_turns, num_part=num_part, qx0=20.3, qy0=20.25, proton_optics='q20')
+tbt = sps.track_SPS(which_context='gpu', install_SC_on_line=True, beta_beat=0.05, plane_for_beta_beat='X', add_non_linear_magnet_errors=True,
                     apply_kinetic_IBS_kicks=False, distribution_type='binomial')
 tbt.to_json(output_dir)
