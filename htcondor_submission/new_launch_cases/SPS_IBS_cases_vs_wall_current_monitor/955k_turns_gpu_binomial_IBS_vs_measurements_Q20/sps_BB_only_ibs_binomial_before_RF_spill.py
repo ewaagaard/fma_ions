5f76f9c839999a507fb185471475958b419500c0
elwaagaa <elias.walter.waagaard@cern.ch>
2024-06-19 20:08:48 +0200

    removed redundant IBS statement

--- htcondor_submission/new_launch_cases/SPS_IBS_cases_vs_wall_current_monitor/955k_turns_gpu_binomial_IBS_vs_measurements_Q20/sps_BB_only_ibs_binomial_before_RF_spill.py
+++ htcondor_submission/new_launch_cases/SPS_IBS_cases_vs_wall_current_monitor/955k_turns_gpu_binomial_IBS_vs_measurements_Q20/sps_BB_only_ibs_binomial_before_RF_spill.py
@@ -12,6 +12,6 @@ num_part = 10_000
 # Test default tracking with space charge on CPU context - then test plotting
 sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=n_turns, num_part=num_part, qx0=20.3, qy0=20.25, proton_optics='q20')
 tbt = sps.track_SPS(which_context='gpu', install_SC_on_line=False, beta_beat=0.1, 
-                    add_non_linear_magnet_errors=True, apply_kinetic_IBS_kicks=True, ibs_step = 5000, distribution_type='binomial',
+                    add_non_linear_magnet_errors=True, apply_kinetic_IBS_kicks=True, distribution_type='binomial',
                     use_binomial_dist_after_RF_spill=False)
 tbt.to_json(output_dir)
