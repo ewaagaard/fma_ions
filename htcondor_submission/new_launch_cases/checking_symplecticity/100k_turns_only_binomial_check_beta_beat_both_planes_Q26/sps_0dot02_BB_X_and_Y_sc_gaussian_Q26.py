75a234aaf4e2d00aba8d0f02a904e5f7ca3e10bf
elwaagaa <elias.walter.waagaard@cern.ch>
2024-06-20 15:17:10 +0200

    new bb cases

---
+++ htcondor_submission/new_launch_cases/checking_symplecticity/100k_turns_only_binomial_check_beta_beat_both_planes_Q26/sps_0dot02_BB_X_and_Y_sc_gaussian_Q26.py
@@ -0,0 +1,15 @@
+"""
+SPS lattice with beta-beat and SC - with GPUs
+"""
+import fma_ions
+import pandas as pd
+output_dir = './'
+
+n_turns = 100_000 
+num_part = 10_000
+
+# Test default tracking with space charge on CPU context - then test plotting
+sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=n_turns, num_part=num_part)
+tbt = sps.track_SPS(which_context='gpu', install_SC_on_line=True, beta_beat=0.1, plane_for_beta_beat='both', add_non_linear_magnet_errors=True,
+                    apply_kinetic_IBS_kicks=False)
+tbt.to_json(output_dir)
