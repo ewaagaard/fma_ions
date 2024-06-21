55aa0993c255ca1fd727d100cb08fd09b3520e51
elwaagaa <elias.walter.waagaard@cern.ch>
2024-06-21 16:13:11 +0200

    fixed beam parameters

--- htcondor_submission/new_launch_cases/SPS_full_flat_bottom_cases/2M_turns_binomial_Pb/sps_BB_space_charge.py
+++ htcondor_submission/new_launch_cases/SPS_full_flat_bottom_cases/2M_turns_binomial_Pb/sps_BB_space_charge.py
@@ -11,6 +11,6 @@ num_part = 10_000
 
 # Test default tracking with space charge on CPU context - then test plotting
 sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=n_turns, num_part=num_part)
-tbt = sps.track_SPS(which_context='gpu', beamParams=beamParams, install_SC_on_line=True, beta_beat=0.05,  
+tbt = sps.track_SPS(which_context='gpu', install_SC_on_line=True, beta_beat=0.05,  
                     add_non_linear_magnet_errors=True, distribution_type='binomial')
 tbt.to_json(output_dir)
