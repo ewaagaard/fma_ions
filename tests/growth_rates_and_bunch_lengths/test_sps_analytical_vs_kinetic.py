c9657e3bd0640a2a0585e19b350530780997debe
elwaagaa <elias.walter.waagaard@cern.ch>
2024-06-27 16:31:36 +0200

    updated tests for IBS growth rates

--- tests/growth_rates_and_bunch_lengths/test_sps_analytical_vs_kinetic.py
+++ tests/growth_rates_and_bunch_lengths/test_sps_analytical_vs_kinetic.py
@@ -4,8 +4,7 @@ Test tracking with kinetic IBS kicks vs Nagaitsev formalism, for SPS set-up
 import fma_ions
 
 # Test default tracking with space charge on GPU context - then test plotting
-sps = fma_ions.SPS_Flat_Bottom_Tracker(num_part=2000, num_turns=100, turn_print_interval=10)
+sps = fma_ions.SPS_Flat_Bottom_Tracker(num_part=2000, num_turns=20_000, turn_print_interval=10)
 
 # First run with ideal lattice, then with beta-beat
-sps.run_analytical_vs_kinetic_emittance_evolution()
-sps.run_analytical_vs_kinetic_emittance_evolution(beta_beat=0.1, add_non_linear_magnet_errors=True)
+df_kick, df_analytical = sps.run_analytical_vs_kinetic_emittance_evolution(return_data=True, show_plot=True)
