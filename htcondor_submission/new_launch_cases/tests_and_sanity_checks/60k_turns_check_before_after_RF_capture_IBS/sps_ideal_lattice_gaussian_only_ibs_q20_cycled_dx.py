8527df6d541d7f2c66b47078bac5829379419974
elwaagaa <elias.walter.waagaard@cern.ch>
2024-07-18 12:12:59 +0200

    new HTCondor submission cases

---
+++ htcondor_submission/new_launch_cases/tests_and_sanity_checks/60k_turns_check_before_after_RF_capture_IBS/sps_ideal_lattice_gaussian_only_ibs_q20_cycled_dx.py
@@ -0,0 +1,14 @@
+"""
+SPS lattice with beta-beat and SC - with GPUs
+"""
+import fma_ions
+output_dir = './'
+
+n_turns = 60_000 
+num_part = 10_000
+
+# Test default tracking with space charge on CPU context - then test plotting
+sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=n_turns, num_part=num_part, qx0=20.3, qy0=20.25, proton_optics='q20')
+tbt = sps.track_SPS(which_context='gpu', install_SC_on_line=False, apply_kinetic_IBS_kicks=True, distribution_type='qgaussian', 
+                    cycle_mode_to_minimize_dx_dpx='dx')
+tbt.to_json(output_dir)
---
+++ htcondor_submission/new_launch_cases/tests_and_sanity_checks/60k_turns_check_before_after_RF_capture_IBS/sps_ideal_lattice_gaussian_only_ibs_q26_normal.py
@@ -0,0 +1,13 @@
+"""
+SPS lattice with beta-beat and SC - with GPUs
+"""
+import fma_ions
+output_dir = './'
+
+n_turns = 60_000 
+num_part = 10_000
+
+# Test default tracking with space charge on CPU context - then test plotting
+sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=n_turns, num_part=num_part)
+tbt = sps.track_SPS(which_context='gpu', install_SC_on_line=False, apply_kinetic_IBS_kicks=True, distribution_type='qgaussian')
+tbt.to_json(output_dir)
