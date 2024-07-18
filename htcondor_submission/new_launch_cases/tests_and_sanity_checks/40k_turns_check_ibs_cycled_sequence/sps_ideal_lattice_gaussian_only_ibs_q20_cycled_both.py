c9674a5b5bd7e7d73a679daaa7fc5f5ff4ed7252
elwaagaa <elias.walter.waagaard@cern.ch>
2024-07-18 16:27:09 +0200

    changed num turns

--- htcondor_submission/new_launch_cases/tests_and_sanity_checks/40k_turns_check_ibs_cycled_sequence/sps_ideal_lattice_gaussian_only_ibs_q20_cycled_both.py
+++ htcondor_submission/new_launch_cases/tests_and_sanity_checks/40k_turns_check_ibs_cycled_sequence/sps_ideal_lattice_gaussian_only_ibs_q20_cycled_both.py
@@ -4,7 +4,7 @@ SPS lattice with beta-beat and SC - with GPUs
 import fma_ions
 output_dir = './'
 
-n_turns = 60_000 
+n_turns = 40_000 
 num_part = 10_000
 
 beamParams = fma_ions.BeamParameters_SPS_Binomial_2016()
@@ -13,5 +13,5 @@ beamParams.Nb = 3.59e9 # take intensity times 10 to see effect more strongly
 # Test default tracking with space charge on CPU context - then test plotting
 sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=n_turns, num_part=num_part, qx0=20.3, qy0=20.25, proton_optics='q20')
 tbt = sps.track_SPS(which_context='gpu', beamParams=beamParams, install_SC_on_line=False, apply_kinetic_IBS_kicks=True, distribution_type='gaussian', 
-                    cycle_mode_to_minimize_dx_dpx='dpx')
+                    cycle_mode_to_minimize_dx_dpx='both')
 tbt.to_json(output_dir)
