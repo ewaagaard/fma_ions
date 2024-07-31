5b2da3f86876d8ac588a749aa6110fd317edd8e8
elias <elias.waagaard@gmail.com>
2024-07-31 18:32:54 +0200

    beta-beat scan

---
+++ htcondor_submission/new_launch_cases/955k_turns_beta_beat_scan/launch_gpu.py
@@ -0,0 +1,26 @@
+"""
+Launcher script to HTCondor for SPS cases with GPUs
+"""
+import fma_ions
+import os
+import pathlib
+
+# Find path of script being run
+dir_path = pathlib.Path(__file__).parent.absolute()
+script_names = ['sps_ideal_lattice_sc_qgaussian.py', 'sps_1percBB_sc_qgaussian.py', 'sps_2percBB_sc_qgaussian.py',
+                'sps_5percBB_sc_qgaussian.py', 'sps_10percBB_sc_qgaussian.py']
+folder_names = ['sps_ideal_lattice_sc_qgaussian', 'sps_1percBB_sc_qgaussian', 'sps_2percBB_sc_qgaussian',
+                'sps_5percBB_sc_qgaussian', 'sps_10percBB_sc_qgaussian']
+string_array = ['sps_ideal_lattice_sc_qgaussian', 'sps_1percBB_sc_qgaussian', 'sps_2percBB_sc_qgaussian',
+                'sps_5percBB_sc_qgaussian', 'sps_10percBB_sc_qgaussian']    
+
+# Instantiate the submitter class and launch the two jobs
+sub = fma_ions.Submitter() 
+
+# Launch the Python scripts in this folder
+for i, script in enumerate(script_names):
+    file_name = os.path.join(dir_path, script)
+    print(f"Submitting {file_name}")
+    sub.submit_GPU(file_name, extra_output_name_str=folder_names[i], number_of_turn_string='955k_turns', job_flavour='nextweek')
+sub.copy_master_plot_script(folder_names, string_array)
+
