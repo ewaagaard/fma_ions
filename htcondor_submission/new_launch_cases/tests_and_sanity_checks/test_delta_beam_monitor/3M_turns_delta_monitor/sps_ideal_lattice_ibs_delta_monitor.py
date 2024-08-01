1f5858f88f1bd66da1e3cc7b0940975731fc5ae2
elias <elias.waagaard@gmail.com>
2024-08-01 19:44:22 +0200

    new htcondor submission cases for delta beam monitor

--- htcondor_submission/new_launch_cases/tests_and_sanity_checks/test_delta_beam_monitor/3M_turns_delta_monitor/sps_ideal_lattice_ibs_delta_monitor.py
+++ htcondor_submission/new_launch_cases/tests_and_sanity_checks/test_delta_beam_monitor/3M_turns_delta_monitor/sps_ideal_lattice_ibs_delta_monitor.py
@@ -1,15 +1,14 @@
 """
-SPS lattice with ideal lattice and SC - with GPUs
+SPS lattice with ideal lattice - with GPUs
 """
 import fma_ions
 import pandas as pd
 output_dir = './'
 
-n_turns = 100_000
+n_turns = 3_000_000
 num_part = 10_000
 
 # Test default tracking with space charge on CPU context - then test plotting
 sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=n_turns, num_part=num_part)
-tbt = sps.track_SPS(which_context='gpu', install_SC_on_line=True, 
-                    apply_kinetic_IBS_kicks=False, distribution_type='binomial')
+tbt = sps.track_SPS(which_context='gpu', install_SC_on_line=False, apply_kinetic_IBS_kicks=True, also_keep_delta_profiles=True)
 tbt.to_json(output_dir)
