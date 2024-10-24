1cc482e62dec4718c4a4ab7605e90faae048e3e0
elwaagaa <elias.walter.waagaard@cern.ch>
2024-10-24 14:28:38 +0200

    cpu run as well

--- htcondor_submission/new_launch_cases/tests_and_sanity_checks/test_small_launch/500_turns_small_tune_scan_protons/launch_cpu_tune_scan.py
+++ htcondor_submission/new_launch_cases/tests_and_sanity_checks/test_small_launch/500_turns_small_tune_scan_protons/launch_cpu_tune_scan.py
@@ -5,18 +5,21 @@ import fma_ions
 import os
 import pathlib
 import numpy as np
+import datetime
 
 # Find path of script being run
 dir_path = pathlib.Path(__file__).parent.absolute()
 
 # Define run files and which parameters to change
-Qy_range = np.arange(26.13, 26.25, 0.01)
-run_files = ['sps_run_{}_tbt.py'.format(i+1) for i in range(len(Qy_range))]
+num_turns = 20 
+Qx = 26.13
+Qy_range = np.arange(26.15, 26.30, 0.01)
+run_files = ['sps_run_{}_tbt_qx_26dot13.py'.format(i+1) for i in range(len(Qy_range))]
 
 # Define script and folder names
 script_names = run_files.copy()
-folder_names = ['sps_Qy_{:.2f}_IBS_BB'.format(Qy_range[i]) for i in range(len(Qy_range))]
-string_array = ['Qy = {:.2f}, IBS with BB'.format(Qy_range[i]) for i in range(len(Qy_range))]    
+folder_names = ['sps_Qx_{:.2f}_Qy_{:.2f}'.format(Qx, Qy_range[i]) for i in range(len(Qy_range))]
+string_array = ['Qy = {:.2f}, Qx = {:.2f} space charge'.format(Qy_range[i], Qx) for i in range(len(Qy_range))]    
 
 # Generate the scripts to be submitted
 for i, run_file in enumerate(run_files):
@@ -29,26 +32,27 @@ for i, run_file in enumerate(run_files):
     '''import fma_ions
 output_dir = './'
 
-n_turns = 20
+n_turns = {}
 num_part = 20_000
 
 
-# Test default tracking with space charge on CPU context - then test plotting
-sps = fma_ions.SPS_Flat_Bottom_Tracker(qy0={:.4f}, num_turns=n_turns, num_part=num_part)
-tbt = sps.track_SPS(ion_type='proton', which_context='cpu', install_SC_on_line=False, beta_beat=0.1, 
-                add_non_linear_magnet_errors=True, apply_kinetic_IBS_kicks=True)
+# Tracking on CPU context
+sps = fma_ions.SPS_Flat_Bottom_Tracker(qx0={:.3f}, qy0={:.3f}, num_turns=n_turns, num_part=num_part)
+tbt = sps.track_SPS(ion_type='proton', which_context='cpu', install_SC_on_line=False, beta_beat=None, 
+                add_non_linear_magnet_errors=False, apply_kinetic_IBS_kicks=False)
 tbt.to_json(output_dir)
-    '''.format(Qy_range[i])
+    '''.format(num_turns, Qx, Qy_range[i])
     )
     run_file.close()
     
     
 # Instantiate the submitter class and launch the jobs
 sub = fma_ions.Submitter() 
+master_job_name = '{:%Y_%m_%d__%H_%M}'.format(datetime.datetime.now())
 
 # Launch the Python scripts in this folder
 for i, script in enumerate(script_names):
     file_name = os.path.join(dir_path, script)
     print(f"Submitting {file_name}")
-    sub.submit_CPU(file_name, extra_output_name_str=folder_names[i], number_of_turn_string='')
+    sub.submit_CPU(file_name, master_job_name=master_job_name, extra_output_name_str=folder_names[i])
 sub.copy_master_plot_script(folder_names, string_array)
