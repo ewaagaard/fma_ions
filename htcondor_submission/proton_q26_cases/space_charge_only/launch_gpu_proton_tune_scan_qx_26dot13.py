d4dfc8f18cc2ce2df37200373afdc0031811513d
elwaagaa <elias.walter.waagaard@cern.ch>
2024-10-24 14:32:43 +0200

    fixed argument

--- htcondor_submission/proton_q26_cases/space_charge_only/launch_gpu_proton_tune_scan_qx_26dot13.py
+++ htcondor_submission/proton_q26_cases/space_charge_only/launch_gpu_proton_tune_scan_qx_26dot13.py
@@ -54,5 +54,5 @@ master_job_name = '{:%Y_%m_%d__%H_%M}'.format(datetime.datetime.now())
 for i, script in enumerate(script_names):
     file_name = os.path.join(dir_path, script)
     print(f"Submitting {file_name}")
-    sub.submit_GPU(file_name, master_job_name=master_job_name, extra_output_name_str=folder_names[i])
+    sub.submit_GPU(file_name, master_job_name=master_job_name, job_name=folder_names[i])
 sub.copy_master_plot_script(folder_names, string_array)
