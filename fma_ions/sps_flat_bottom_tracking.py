c051cc81bb9e375ecaac8024c62158d70a93cd26
elwaagaa <elias.walter.waagaard@cern.ch>
2024-11-28 16:23:22 +0100

    Updated decimals and print of losses

--- fma_ions/sps_flat_bottom_tracking.py
+++ fma_ions/sps_flat_bottom_tracking.py
@@ -445,12 +445,12 @@ class SPS_Flat_Bottom_Tracker:
                 print('\nTracking turn {}'.format(turn))            
                 
                 if add_tune_ripple:
-                    print('kqf = {:.7f}, kqf = {:.7f}'.format(line.vars['kqf']._value, line.vars['kqd']._value))
+                    print('kqf = {:.8f}, kqf = {:.8f}'.format(line.vars['kqf']._value, line.vars['kqd']._value))
 
                 if particles.state[particles.state <= 0].size > 0:
                     print('Total lost particles: {}'.format(len(particles.state[particles.state <= 0])))
                     loss_type, loss_count = np.unique(particles.state, return_counts=True)
-                    print('Loss types: {}\nwith occurrence {}'.format(loss_type, loss_count))
+                    print('Loss types: {}, with occurrence {}'.format(loss_type, loss_count))
                     #print('Lost particle state: most common code: "-{}" for {} particles out of {} lost in total'.format(np.bincount(np.abs(particles.state[particles.state <= 0])).argmax(),
                     #                                                                    np.max(np.bincount(np.abs(particles.state[particles.state <= 0]))),
                     #                                                                    len(particles.state[particles.state <= 0])))
