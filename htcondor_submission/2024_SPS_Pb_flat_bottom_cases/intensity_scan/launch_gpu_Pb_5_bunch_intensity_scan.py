55afd24da3c31c1501875ec9f2da48786d095cb1
elwaagaa <elias.walter.waagaard@cern.ch>
2024-12-16 18:13:43 +0100

    fixed name

--- htcondor_submission/2024_SPS_Pb_flat_bottom_cases/intensity_scan/launch_gpu_Pb_5_bunch_intensity_scan.py
+++ htcondor_submission/2024_SPS_Pb_flat_bottom_cases/intensity_scan/launch_gpu_Pb_5_bunch_intensity_scan.py
@@ -11,8 +11,8 @@ import datetime
 dir_path = pathlib.Path(__file__).parent.absolute()
 
 # Define run files and which parameters to change
-master_name = 'Q26_Pb_ions_50Hz_amplitude_scan'
-num_turns = 130_000 # corresponds to 3s for SPS ions at flat bottom
+master_name = 'Q26_Pb_ions_bunch_intensity_scan'
+num_turns = 2_000_000 # corresponds to 48s for SPS ions at flat bottom
 Qy = 26.19
 Qx = 26.31
 
