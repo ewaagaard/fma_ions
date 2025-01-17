ddca39b3e2f420cba8e6a824511c44ae1eefe635
elwaagaa <elias.walter.waagaard@cern.ch>
2025-01-17 10:32:42 +0100

    GPU test with longer runtime

--- htcondor_submission/tests_and_sanity_checks/test_small_launch/500_turns_test_gpu/sps_BB_only_ibs.py
+++ htcondor_submission/tests_and_sanity_checks/test_small_launch/500_turns_test_gpu/sps_BB_only_ibs.py
@@ -9,6 +9,5 @@ num_part = 10_000
 
 # Test default tracking with space charge on CPU context - then test plotting
 sps = fma_ions.SPS_Flat_Bottom_Tracker(num_turns=n_turns, num_part=num_part)
-tbt = sps.track_SPS(which_context='gpu', install_SC_on_line=False, beta_beat=0.1, 
-                    add_non_linear_magnet_errors=True, apply_kinetic_IBS_kicks=True, ibs_step = 5000)
+tbt = sps.track_SPS(which_context='gpu', install_SC_on_line=False)
 tbt.to_json(output_dir)
