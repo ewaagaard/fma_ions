b0fadd62f6b10d93a20abb209727023039c10b06
elwaagaa <elias.walter.waagaard@cern.ch>
2025-02-14 16:29:10 +0100

    removed from cernbox due to space

--- cases/sps_Pb_ideal_lattice/fma_with_beta_beat/sps_fma_Pb_on_momentum_ideal_lattice_bb_5perc.py
+++
@@ -1,8 +0,0 @@
-"""
-Generate SPS Pb ion FMA plot - ideal lattice, no momentum offset
-- with 5 % beta-beat
-"""
-import fma_ions
-
-fma_sps = fma_ions.FMA(output_folder='output_Pb_on_momentum_ideal_lattice_5_per_bb', z0=0., n_linear=200)
-fma_sps.run_SPS_with_beta_beat(load_tbt_data=False, beta_beat=0.05)
