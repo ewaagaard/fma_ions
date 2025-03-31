fc01b6d4917e05e75ede3bde4317c273a89d0e06
elias <elias.waagaard@gmail.com>
2025-03-31 11:29:30 +0200

    Tune ripple freq

--- htcondor_submission/fma_plots/sps_pb_tune_ripple/launch_fma_sps_pb_ripple_freq_scan.py
+++ htcondor_submission/fma_plots/sps_pb_tune_ripple/launch_fma_sps_pb_ripple_freq_scan.py
@@ -12,7 +12,7 @@ dir_path = pathlib.Path(__file__).parent.absolute()
 
 # Define run files and which parameters to change
 master_name = 'Q26_Pb_FMA_on_momentum_ideal_lattice_ripple_scan'
-ripple_freq = [10., 50., 150., 300., 600., 1200.]
+ripple_freq = [0.0, 10., 50., 150., 300., 600., 1200.]
 Qx = 26.31
 Qy = 26.25
 
--- htcondor_submission/fma_plots/sps_pb_tune_ripple/launch_fma_sps_pb_ripple_freq_scan_magnet_errors_beta_beat.py
+++ htcondor_submission/fma_plots/sps_pb_tune_ripple/launch_fma_sps_pb_ripple_freq_scan_magnet_errors_beta_beat.py
@@ -11,15 +11,15 @@ import datetime
 dir_path = pathlib.Path(__file__).parent.absolute()
 
 # Define run files and which parameters to change
-master_name = 'Q26_Pb_FMA_on_momentum_ideal_lattice_ripple_scan_magnet_errors'
+master_name = 'Q26_Pb_FMA_on_momentum_ideal_lattice_ripple_scan_magnet_errors_beta_beat'
 ripple_freq = [0., 10., 50., 150., 300., 600., 1200.]
 Qx = 26.31
 Qy = 26.25
 
 run_files, folder_names, string_array = [], [] , []
 for i in range(len(ripple_freq)):
-    run_files.append('sps_run{}_ripple_magnet_errors.py'.format(i+1))
-    folder_names.append('sps_Qx_{:.2f}_Qy_{:.2f}_ripple_{:.3e}_Hz_ripple_magnet_errors'.format(Qx, Qy, ripple_freq[i]))
+    run_files.append('sps_run{}_ripple_magnet_errors_beta_beat.py'.format(i+1))
+    folder_names.append('sps_Qx_{:.2f}_Qy_{:.2f}_ripple_{:.3e}_Hz_ripple_magnet_errors_beta_beat'.format(Qx, Qy, ripple_freq[i]))
     string_array.append('Qx = {:.2f}, Qy = {:.2f}, ripple = {:.3e}'.format(Qx, Qy, ripple_freq[i]))
 
 # Generate the scripts to be submitted
@@ -38,7 +38,7 @@ output_dir = './'
 
 # Tracking on GPU context
 fma_sps = fma_ions.FMA(n_linear=200)
-tbt = fma_sps.run_SPS(qx0={:.3f}, qy0={:.3f}, which_context = 'gpu', add_non_linear_magnet_errors=True, add_tune_ripple=True, ripple_freqs=np.array([{}]))
+tbt = fma_sps.run_SPS(qx0={:.3f}, qy0={:.3f}, which_context = 'gpu', add_non_linear_magnet_errors=True, add_tune_ripple=True, ripple_freqs=np.array([{}]), add_beta_beat=True)
 tbt.to_json(output_dir)
     '''.format(Qx, Qy, ripple_freq[i])
     )
